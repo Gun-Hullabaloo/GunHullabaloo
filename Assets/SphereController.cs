8cf937c2782fb7b287657ce5abad74eb7f298104
장승호 <hoosong0235@kaist.ac.kr>
2023-10-26 09:41:40 -0400

    PlayerController, SphereController

---
+++ Assets/SphereController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereController : MonoBehaviour
+{
+    public float dir;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.Translate(10f * dir * Time.deltaTime, 0f, 0f);
+    }
+}
